1580f56ff25f90cf1c30a1af3f8f7105dfd6cdfe
Raivo Laanemets <raivo@infdot.com>
2022-06-25 02:14:21 +0300

    Extracting Emscripten files to src/swipl directory

--- wasm/extract-wasm.sh
+++ wasm/extract-wasm.sh
@@ -5,7 +5,7 @@
 set -o errexit
 set -o xtrace
 
-mkdir -p dist/swipl
-cp wasm/swipl-devel/build.wasm/src/swipl-web.data dist/swipl/swipl-web.data
-cp wasm/swipl-devel/build.wasm/src/swipl-web.wasm dist/swipl/swipl-web.wasm
-cp wasm/swipl-devel/build.wasm/src/swipl-web.js dist/swipl/swipl-web.js
+mkdir -p src/swipl
+cp wasm/swipl-devel/build.wasm/src/swipl-web.data src/swipl/swipl-web.data
+cp wasm/swipl-devel/build.wasm/src/swipl-web.wasm src/swipl/swipl-web.wasm
+cp wasm/swipl-devel/build.wasm/src/swipl-web.js src/swipl/swipl-web.js
