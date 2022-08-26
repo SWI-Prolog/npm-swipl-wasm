afea9e7b05cb0d251e35cfbfc66a4154fa0c7add
Raivo Laanemets <raivo@infdot.com>
2022-08-26 22:43:20 +0300

    New wrapper

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,47 @@
-import SWIPL from "./swipl/swipl-web";
-
-SWIPL({
+const defaultOverrides: Partial<EmscriptenModule> = {
   noInitialRun: true,
-  arguments: ["swipl", "-x", "src/wasm-preload/boot.prc", "--nosignals"],
-  locateFile: function (file: any) {
-    console.log("locate " + file);
-    //return path.join(__dirname, "wasm", "dist", file);
+  arguments: [],
+};
+
+const defaultNodeOverrides: Partial<EmscriptenModule> = {
+  locateFile: (url) => {
+    if (url.endsWith(".data")) {
+      return `${__dirname}/swipl/swipl-web.data`;
+    } else if (url.endsWith(".wasm")) {
+      return `${__dirname}/swipl/swipl-web.wasm`;
+    }
+    return url;
   },
-  logReadFiles: true,
-}).then(async (module: any) => {
-  module.FS.writeFile("hello_world", "abc");
-  module.prolog.call_string("consult(hello_world).");
-  console.log("SWI-Prolog WebAssembly ready");
-});
+};
+
+export type SWIPLModule = {
+  FS: typeof FS;
+} & EmscriptenModule;
+
+/**
+ * Initializes the web version of SWI-Prolog. The
+ * web version uses emulated virtual filesystem while accessing files.
+ */
+export const initializeWeb = async (
+  moduleOverrides: Partial<EmscriptenModule> = {}
+) => {
+  const overrides = { ...defaultOverrides, ...moduleOverrides };
+  const { default: SWIPL } = await import("./swipl/swipl-web");
+  return SWIPL(overrides) as Promise<SWIPLModule>;
+};
+
+/**
+ * Initializes the Node version of SWI-Prolog. The Node
+ * version uses Node.js filesystem APIs while accessing files.
+ */
+export const initializeNode = async (
+  moduleOverrides: Partial<EmscriptenModule> = {}
+) => {
+  const overrides = {
+    ...defaultOverrides,
+    ...defaultNodeOverrides,
+    ...moduleOverrides,
+  };
+  const { default: SWIPL } = await import("./swipl/swipl");
+  return SWIPL(overrides) as Promise<SWIPLModule>;
+};
