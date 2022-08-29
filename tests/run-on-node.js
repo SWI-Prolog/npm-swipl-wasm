29cc995e14c941a147fe5920319c84ba9ed47752
Raivo Laanemets <raivo@infdot.com>
2022-08-29 14:46:09 +0300

    Rewrote Node.js example

--- tests/run-on-node.js
+++ tests/run-on-node.js
@@ -1,6 +1,21 @@
-const { initializeNode } = require('../');
+const path = require('path');
+const SWIPL = require('swipl-wasm/swipl');
 
 (async () => {
-  const swipl = await initializeNode();
-  console.log(swipl.prolog.query("member(X, [1, 2, 3]).").once().X);
+  // This helps to find the correct locations of the
+  // .data and .wasm files.
+  const swiplModuleLocation = require.resolve('swipl-wasm/swipl');
+  const swipl = await SWIPL({
+    locateFile: (url) => {
+      // These are common with the web version.
+      if (url === 'swipl-web.data') {
+        return path.join(path.dirname(swiplModuleLocation), 'swipl-web.data');
+      } else if (url === 'swipl-web.wasm') {
+        return path.join(path.dirname(swiplModuleLocation), 'swipl-web.wasm');
+      }
+      return url;
+    }
+  });
+  // See https://swi-prolog.discourse.group/t/swi-prolog-in-the-browser-using-wasm/5650/1
+  console.log(swipl.prolog.query("member(X, [a, b, c]).").once().X);
 })();
