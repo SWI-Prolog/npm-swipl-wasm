a66cb67b382e9c0b54734a991ce32dfd9e94b93d
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2022-08-30 16:37:38 +1000

    chore: work on build scripts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import SWIPL, { SwiplOptions, SwiplModule } from './swipl';
+import SWIPL, { SwiplOptions, SwiplModule } from './swipl/swipl';
 import { locateFile } from './locateFile';
 
 export default function(args: SwiplOptions): SwiplModule {
   return SWIPL({ locateFile, noInitialRun: true, arguments: ["-q"], ...args });
 }
+
+export { SwiplOptions, SwiplModule } from './swipl/swipl';
