e7bce22a45003bba2bd7c47c70873a29b9d59a7f
Raivo Laanemets <raivo@infdot.com>
2022-08-31 14:39:55 +0300

    Included GMP in build

--- docker/build-all.sh
+++ docker/build-all.sh
@@ -7,9 +7,17 @@ source /emsdk/emsdk_env.sh
 
 # ZLIB
 
-cd /zlib-1.2.12
+cd /zlib-$ZLIB_VERSION
 emconfigure ./configure
 EMCC_CFLAGS=-Wno-deprecated-non-prototype emmake make
+
+# GMP
+
+cd /gmp-$GMP_VERSION
+mkdir /gmp
+emconfigure ./configure --host=none --disable-assembly --prefix=/gmp
+make
+make install
 
 # SWI-Prolog
 
@@ -17,12 +25,12 @@ mkdir /swipl-devel/swipl.wasm
 cd /swipl-devel/swipl.wasm
 cmake -DCMAKE_TOOLCHAIN_FILE=/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake \
   -DCMAKE_BUILD_TYPE=Release \
-  -DZLIB_LIBRARY=/zlib-1.2.12/libz.a \
-  -DZLIB_INCLUDE_DIR=/zlib-1.2.12 \
+  -DZLIB_LIBRARY=/zlib-$ZLIB_VERSION/libz.a \
+  -DZLIB_INCLUDE_DIR=/zlib-$ZLIB_VERSION \
   -DINSTALL_DOCUMENTATION=OFF \
   -DMULTI_THREADED=OFF \
   -DUSE_SIGNALS=OFF \
-  -DUSE_GMP=OFF \
+  -DGMP_ROOT=/gmp \
   -DBUILD_SWIPL_LD=OFF \
   -DSWIPL_PACKAGES=OFF \
   -G Ninja ..
