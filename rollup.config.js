5eb23f67c8d45fa6501c75d08b823308a6a0939c
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2022-09-05 14:06:46 +1000

    WIP

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,15 @@
-import pkg from "./package.json";
+// import pkg from "./package.json";
 import typescript from "rollup-plugin-typescript2";
 
 export default {
-  input: "./src/index.ts",
-  output: [
-    {
-      file: pkg.main,
-      format: "cjs",
-    },
-    {
-      file: pkg.module,
-      entryFileNames: "[name].es.js",
-      format: "esm",
-    },
-    {
-      dir: "dist",
-      entryFileNames: "[name].mjs",
-      format: "esm",
-      preserveModules: true,
-    },
-    {
-      dir: "umd",
-      format: "umd",
-      name: "SolidClient",
-    },
-  ],
+  input: 'src/index.ts',
+  output: {
+    file: 'bundle/bundle.js',
+    format: 'cjs'
+  },
   plugins: [
-    typescript({
+    typescript(
+      {
       // Use our own version of TypeScript, rather than the one bundled with the plugin:
       typescript: require("typescript"),
       tsconfigOverride: {
@@ -36,13 +19,54 @@ export default {
           module: "esnext",
         },
       },
-    }),
-  ],
-  external: [
-    "cross-fetch",
-    "http-link-header",
-    "@rdfjs/dataset",
-    "n3",
-    "jsonld",
+    }
+    ),
   ],
 };
+
+
+// export default {
+//   input: "./src/index.ts",
+//   output: [
+//     {
+//       file: pkg.main,
+//       format: "cjs",
+//     },
+//     {
+//       file: pkg.module,
+//       entryFileNames: "[name].es.js",
+//       format: "esm",
+//     },
+//     {
+//       dir: "dist",
+//       entryFileNames: "[name].mjs",
+//       format: "esm",
+//       preserveModules: true,
+//     },
+//     {
+//       dir: "umd",
+//       format: "umd",
+//       name: "SolidClient",
+//     },
+//   ],
+  // plugins: [
+  //   typescript({
+  //     // Use our own version of TypeScript, rather than the one bundled with the plugin:
+  //     typescript: require("typescript"),
+  //     tsconfigOverride: {
+  //       // Exclude tests:
+  //       exclude: ["**/*.test.ts"],
+  //       compilerOptions: {
+  //         module: "esnext",
+  //       },
+  //     },
+  //   }),
+  // ],
+//   external: [
+//     "cross-fetch",
+//     "http-link-header",
+//     "@rdfjs/dataset",
+//     "n3",
+//     "jsonld",
+//   ],
+// };
