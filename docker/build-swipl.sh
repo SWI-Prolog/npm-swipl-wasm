ddbad057134220a6c6be1322f4b8a6b9872351c4
Jan Wielemaker <J.Wielemaker@vu.nl>
2022-09-23 12:23:07 +0200

    Sync the cmake option with the current status and use build.wasm rather than swipl.wasm as build directory to avoid confusion.

--- docker/build-swipl.sh
+++ docker/build-swipl.sh
@@ -7,17 +7,13 @@ source /emsdk/emsdk_env.sh
 
 # SWI-Prolog
 
-mkdir /swipl-devel/swipl.wasm
-cd /swipl-devel/swipl.wasm
+mkdir /swipl-devel/build.wasm
+cd /swipl-devel/build.wasm
 cmake -DCMAKE_TOOLCHAIN_FILE=/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake \
   -DCMAKE_BUILD_TYPE=Release \
   -DZLIB_LIBRARY=/zlib-$ZLIB_VERSION/libz.a \
   -DZLIB_INCLUDE_DIR=/zlib-$ZLIB_VERSION \
-  -DINSTALL_DOCUMENTATION=OFF \
-  -DMULTI_THREADED=OFF \
-  -DUSE_SIGNALS=OFF \
   -DGMP_ROOT=/gmp \
-  -DSTATIC_EXTENSIONS=ON \
-  -DBUILD_SWIPL_LD=OFF \
+  -DINSTALL_DOCUMENTATION=OFF \
   -G Ninja ..
 ninja
