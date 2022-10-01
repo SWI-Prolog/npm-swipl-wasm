e2ef0173875f87ac0fcab7d36db58faf6c4d9557
Raivo Laanemets <raivo@infdot.com>
2022-10-01 20:58:02 +0300

    Simplified project: removed isomorphic support (for maximum compatibility with node versions), lint (for now), typescript sources

--- src/locateFile-browser.ts
+++
@@ -1,8 +0,0 @@
-export function locateFile (url: string) {
-  if (url === "swipl-web.data") {
-    return "/dist/swipl/swipl-web.data";
-  } else if (url === "swipl-web.wasm") {
-    return "/dist/swipl/swipl-web.wasm";
-  }
-  return url;
-}
