9e23963cc827c5baedd25758c602b72283b58345
Raivo Laanemets <raivo@infdot.com>
2022-10-05 20:04:23 +0300

    More tests

--- tests/node.js
+++ tests/node.js
@@ -22,4 +22,47 @@ describe("SWI-Prolog WebAssembly on Node.js", () => {
       "a"
     );
   });
+
+  it("should run query with arguments", async () => {
+    const swipl = await SWIPL({ arguments: ["-q"] });
+    assert.strictEqual(
+      swipl.prolog.query("member(X, Y).", { Y: ["a", "b", "c"] }).once().X,
+      "a"
+    );
+  });
+
+  it("should run failing query", async () => {
+    const swipl = await SWIPL({ arguments: ["-q"] });
+    const response = swipl.prolog.query("true").once();
+    assert.strictEqual(response.$tag, "bindings");
+  });
+
+  it("should run throwing query", async () => {
+    const swipl = await SWIPL({ arguments: ["-q"] });
+    const response = swipl.prolog.query("throw(error(test, _))").once();
+    assert.strictEqual(response.error, true);
+  });
+
+  it("should eval javascript", async () => {
+    global.wasRun = false;
+    const swipl = await SWIPL({ arguments: ["-q"] });
+    await swipl.prolog.forEach("js_run_script(Script)", {
+      Script: `global.wasRun = true;`,
+    });
+    assert.strictEqual(global.wasRun, true);
+  });
+
+  it("should query SWI-Prolog version", async () => {
+    const swipl = await SWIPL({ arguments: ["-q"] });
+    const version = swipl.prolog
+      .query("current_prolog_flag(version, Version)")
+      .once().Version;
+    assert.ok(version >= 80517);
+  });
+
+  it("should have predictable term conversion", async () => {
+    const swipl = await SWIPL({ arguments: ["-q"] });
+    const atom = swipl.prolog.query("X = atom").once().X;
+    assert.strictEqual(atom, "atom");
+  });
 });
