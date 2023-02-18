f4ae8e3dc9658bf51d7e219388c4497a130967f2
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-02-18 18:53:05 +1100

    chore: add docs

--- examples/generation/dist/main.ts
+++ examples/generation/dist/main.ts
@@ -1,7 +1,7 @@
 import SWIPL from './max';
 
 async function main() {
-  const Module = await SWIPL({ print: e => { console.log(e) } });
+  const Module = await SWIPL();
   const res = Module.prolog.query('max(1, 2, 3).');
   console.log(res.next())
 }
